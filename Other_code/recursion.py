5595b2e136c3d74debdec713cf86c40d1c557c28
catsymptote <catsymptote@gmail.com>
2019-02-21 09:50:09 +0100

    Using list and for. Still not recursive.

--- Other_code/recursion.py
+++ Other_code/recursion.py
@@ -5,9 +5,24 @@ Created on Thu Feb 21 09:24:57 2019
 
 @author: catsymptote
 """
+"""
+lst = []
+
+def f(x):
+    print(x)
+    if len(lst) < 10:
+        x = x**3
+        lst.append(x)
+        return f(x)
+
+f(2)
+"""
 
 def f(x):
     return x**3
 
-for i in range(1,10):
-    print("f({0}) = {1}".format(i, f(i)))
+lst = [2]
+
+for i in range(4):
+    lst.append(f(lst[len(lst) -1]))
+print(lst)
