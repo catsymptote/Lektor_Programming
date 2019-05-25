6f4dc31a061dd8e904e987438d9db2a2b5229d8f
catsymptote <catsymptote@gmail.com>
2019-05-26 00:07:11 +0200

    Oblig 3 init.

--- Oblig_3/1a1.py
+++ Oblig_3/1a1.py
@@ -1,13 +1,15 @@
-def f1(x):
+def f(x):
     return x**4 - 5*x**2 - 2*x + 3
 
-def df1(x):
+def df(x):
     return 4*x**3 - 10*x - 2
 
 def newtons_method(x):
-    for i in range(20):
-        x = x - f1(x) / df1(x)
+    for i in range(100):
+        x = x - f(x) / df(x)
     return x
 
 x = newtons_method(0)
-print("(x, y) : (" + str(x) + ", " + str(f1(x)) + ")")
+print("(x, y) : (" + str(x) + ", " + str(f(x)) + ")")
+
+##  https://www.wolframalpha.com/input/?i=x**4+-+5*x**2+-+2*x+%2B+3
